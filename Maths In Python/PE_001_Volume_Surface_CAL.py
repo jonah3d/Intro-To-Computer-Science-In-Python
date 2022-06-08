57511d27e648d38d5debc6273992d308fc45bbd9
jonah3d <84209915+jonah3d@users.noreply.github.com>
2022-06-08 22:26:00 +0200

    Sphere surface and volume calculator
    Using the python we plot out the formula used to calculate the volume and surface area of a sphere and ask the user for an input.

---
+++ Maths In Python/PE_001_Volume_Surface_CAL.py
@@ -0,0 +1,27 @@
+﻿#Write a program to calculate the volume and surface area of a sphere from its radius,given as input.
+#Useful formulas
+
+#V = 4/3πr3 
+#A = 4πr2
+
+#Algorithm
+
+#.
+#.
+#.
+#.
+
+# We ask th the user to provide the radius of the sphere.
+# Then we apply the formula
+
+import math
+
+def main():
+    r = int(input("Input the radius of the sphere: "))
+    v = (4/3) * math.pi * r**3
+    a = (4*math.pi)*r**2 
+    print("The volume of the sphere is",v) 
+    print("The area of the sphere is",a)
+
+main()
+ 
