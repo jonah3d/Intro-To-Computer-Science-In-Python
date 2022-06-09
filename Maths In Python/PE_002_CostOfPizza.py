682d5b220f62c73f6fbc0efb48591cef359ad504
jonah3d <84209915+jonah3d@users.noreply.github.com>
2022-06-09 12:25:10 +0200

    Price of pizza
    Calculating the price of pizza

---
+++ Maths In Python/PE_002_CostOfPizza.py
@@ -0,0 +1,29 @@
+﻿#Write a program that calculates the cost per square cm of a circular pizza, given its diameter and price.
+#The formula for area of a circle 
+# A = πr2
+
+
+#ALGORITHM
+#
+#
+#
+#
+
+#Indicate the price/square 
+#Ask the user for his diamter and divide it by 2.
+#Tell him the prize of his pizza based on his size
+
+import math
+
+def main():
+    d = float(input("What is the diameter of your pizza: "))
+    r = d//2
+    a = math.pi * r**2
+    shareprice = 1.2
+    price = a * shareprice 
+    print("Share Price is",shareprice)
+    print("The Price of pizza is",price)
+
+main()
+
+    
